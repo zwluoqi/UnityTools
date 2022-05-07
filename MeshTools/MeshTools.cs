f69009d471b38f77d5c51dfeb6a2de72fc7d1237
zwluoqi <zwluoqi@yeah.net>
2022-05-07 21:17:35 +0800

    Mesh tools

---
+++ MeshTools/MeshTools.cs
@@ -0,0 +1,13 @@
+﻿// using UnityEditor;
+// using UnityEngine;
+//
+// namespace UnityTools.MeshTools
+// {
+//     public class MeshTools
+//     {
+//         public static bool SaveMesh(Mesh mesh, string path)
+//         {
+//             AssetDatabase.CreateAsset(mesh,path);
+//         }
+//     }
+// }
