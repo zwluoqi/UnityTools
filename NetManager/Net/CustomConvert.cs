67057c9d124f199fb6782bd593ff457d733daacd
zhouwei <zwluoqi@yeah.net>
2023-07-18 12:01:50 +0800

    socket网络框架

---
+++ NetManager/Net/CustomConvert.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Text;
+
+namespace NetWork.Layer
+{
+    
+    public class CustomConvert
+    {
+        public static string ToObject(int protoId, byte[] receiveBuffer, int i, int ahRequiredLength)
+        {
+            return Encoding.UTF7.GetString(receiveBuffer, i, ahRequiredLength);
+        }
+
+        public static void Serialize(MemoryStream ms, object msg)
+        {
+            ms.Write(Encoding.UTF7.GetBytes((string)msg));
+        }
+    }
+}
---
+++ NetManager/Net/Packet.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace NetWork.Layer
+{
+    
+    public enum OpDefine{
+        CsHeartBeat
+    }
+
+    public class Packet
+    {
+        public int nOpCode;
+        public object kBody;
+
+
+        public Packet()
+        {
+            nOpCode = 0;
+            kBody = null;
+        }
+
+        public Packet(int code, object buff)
+        {
+            nOpCode = code;
+            kBody = buff;
+        }
+    }
+}
+
